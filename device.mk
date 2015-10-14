8d6ce6edf62ff1dbc8079ae40db37d577f9a1ba3
Willi Ye <williye97@gmail.com>
2015-10-14 19:49:59 +0200

    Revert "Don't build ant radio"
    This reverts commit d90d2f8e09fca1d6fad77e7f4efc484c656905ca.

--- device.mk
+++ device.mk
@@ -85,10 +85,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 # ANT+
-#PRODUCT_PACKAGES += \
-#    AntHalService \
-#    com.dsi.ant.antradio_library \
-#    libantradio
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library \
+    libantradio
 
 # Audio
 PRODUCT_COPY_FILES += \
